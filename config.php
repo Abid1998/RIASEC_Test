e0cdc6ef76699de182584a07d3b28480a3a08e9a
Mohd Abid <101179146+Abid1998@users.noreply.github.com>
2023-09-16 11:31:32 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+error_reporting(0);
+ini_set('display_errors', 0);
+ini_set('display_errors', 0);
+$servername = "ServerName";
+$username = "Username";
+$password = "Password";
+$dbname = "DatabaseName";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
